aa86d817b50e671cc24e4b47608c6d9a9668c83f
SilicalNZ <SilicalNZ@gmail.com>
2023-06-30 22:40:13 +0200

    - Include python_requires

--- setup.py
+++ setup.py
@@ -7,12 +7,13 @@ with open("requirements.txt") as fp:
 
 setuptools.setup(
     name="TapTitans2py",
-    version="1.4.0",
+    version="1.4.1",
     license="MIT",
     url="https://gihtub.com/SilicalNZ/TapTitans2py",
     description="A Pythonic wrapper for the Tap Titans 2 API",
     long_description="A Pythonic wrapper for the Tap Titans 2 API",
     author="SilicalNZ@gmail.com",
     install_requires=requirements,
+    python_requires=">=3.10",
     packages=["tap_titans.abcs", "tap_titans.handlers", "tap_titans.models", "tap_titans.providers", "tap_titans.utils"],
 )
