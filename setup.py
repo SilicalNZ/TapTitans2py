c5909ebe710177dc0b800153f3ad8582723aa2a2
SilicalNZ <SilicalNZ@gmail.com>
2024-03-04 13:39:21 +0100

    🐛 Include new models directory in the packages

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("requirements.txt") as fp:
 
 setuptools.setup(
     name="TapTitans2py",
-    version="2.0.0",
+    version="2.0.1",
     license="MIT",
     url="https://gihtub.com/SilicalNZ/TapTitans2py",
     description="A Pythonic wrapper for the Tap Titans 2 API",
@@ -15,5 +15,14 @@ setuptools.setup(
     author="SilicalNZ@gmail.com",
     install_requires=requirements,
     python_requires=">=3.10",
-    packages=["tap_titans.abcs", "tap_titans.handlers", "tap_titans.models", "tap_titans.providers", "tap_titans.utils"],
+    packages=[
+        "tap_titans.abcs",
+        "tap_titans.handlers",
+        "tap_titans.models",
+        "tap_titans.providers",
+        "tap_titans.utils",
+        "tap_titans.models.clan",
+        "tap_titans.models.player",
+        "tap_titans.models.raid",
+    ],
 )
